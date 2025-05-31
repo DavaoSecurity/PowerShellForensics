4e6348621cacb836af2cc38c8eb747de5c2f411d
Davao Cyber Security <nat@davaosecurity.com>
2025-05-31 14:47:34 +0800

    Update bkdoor.ps1

--- bkdoor.ps1
+++ bkdoor.ps1
@@ -2,7 +2,7 @@
 # run as Administrator
 
 # Define the output file path
-$outputFile = "C:\BackdoorScanReport.txt"
+$outputFile = "C:/Users/Nathan/Desktop/bkdoor.txt"
 
 # Function to write header to the report
 function Write-Header {
